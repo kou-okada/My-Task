0e55b7ea5abcc5b2867124c0f4ac0dc3163bfb1d
kou-okada <k.okada@goalist.co.jp>
2022-04-22 13:23:57 +0900

    Complete FifthTask.java

--- src/FifthTask.java
+++ src/FifthTask.java
@@ -3,7 +3,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class FifthTask {
 	public static void main(String[] args) {
@@ -16,35 +20,70 @@ public class FifthTask {
 	}
 
 	private static void printSales(List<String[]> salesItem, List<String[]> salesList) {
+		Calendar date = Calendar.getInstance();
+		Map<String, Integer> salesMaps = new HashMap<>();
+		String salesDate;
+		String startDate = salesList.get(1)[0];
+		String[] startDateArr = startDate.split("/");
 
-		String[] listColumns = salesList.get(0);
-		String itemCode;
-		String date;
-		String listCode;
-		int price;
-		int itemCount;
-		int sales;
+		int[] dateArray = Stream.of(startDateArr).mapToInt(Integer::parseInt).toArray();
+		int sales = 0;
+		int tmp = 0;
+		int itemCost = 0;
+		int itemNums = 0;
 
-		System.out.printf("%s,売り上げ総額\n", listColumns[0]);
-		for (int rowIdx = 1; rowIdx < salesList.size(); rowIdx++) {
+		date.set(dateArray[0], dateArray[1], dateArray[2]);
 
-			date = salesList.get(rowIdx)[0];
-			listCode = salesList.get(rowIdx)[1];
-			itemCount = Integer.parseInt(salesList.get(rowIdx)[2]);
-
-			for (int itemIdx = 1; itemIdx < salesItem.size(); itemIdx++) {
-
-				itemCode = salesItem.get(itemIdx)[0];
-				price = Integer.parseInt(salesItem.get(itemIdx)[2]);
-
-				if (listCode.equals(itemCode)) {
-					sales = price * itemCount;
-					System.out.printf("%s,%d円\n", date, sales);
-				}
-
-			}
+		while (!((date.get(Calendar.MONTH) == 2) && (date.get(Calendar.DATE) == 16))) {
+			salesMaps.put(getDate(date), 0);
+			date.add(Calendar.DAY_OF_MONTH, 1);
 		}
 
+		for (int rowIdx = 1; rowIdx < salesList.size(); rowIdx++) {
+
+			String itemCode = salesList.get(rowIdx)[1];
+			salesDate = salesList.get(rowIdx)[0];
+
+			for (int itemIdx = 1; itemIdx < salesItem.size(); itemIdx++) {
+				itemCost = Integer.parseInt(salesItem.get(itemIdx)[2]);
+				itemNums = Integer.parseInt(salesList.get(rowIdx)[2]);
+
+				if (itemCode.equals(salesItem.get(itemIdx)[0])) {
+					sales = itemCost * itemNums;
+				}
+			}
+
+			if (salesMaps.containsKey(salesDate)) {
+				tmp = salesMaps.get(salesDate) + sales;
+				salesMaps.put(salesDate, tmp);
+			}
+
+		}
+
+		date.set(2020, 1, 15);
+		System.out.println("販売日,売上総額");
+		while (!((date.get(Calendar.MONTH) == 2) && (date.get(Calendar.DATE) == 16))) {
+			System.out.printf("%s,%d円\n", getDate(date), salesMaps.get(getDate(date)));
+			date.add(Calendar.DAY_OF_MONTH, 1);
+		}
+
+	}
+
+	private static String getDate(Calendar cal) {
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		int day = cal.get(Calendar.DATE);
+		String dayStr = Integer.valueOf(day).toString();
+		String monthStr = Integer.valueOf(month).toString();
+		if (day < 10) {
+			dayStr = "0" + day;
+		}
+
+		if (month < 10) {
+			monthStr = "0" + month;
+		}
+
+		return year + "/" + monthStr + "/" + dayStr;
 	}
 
 	private static List<String[]> readCSV(String filepath) {
